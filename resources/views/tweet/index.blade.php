a805d7640c092a24f216fd5c03a1ea9049741b4d
ankomochi114 <s.kotomi.0114@gmail.com>
2022-07-29 20:21:08 +0900

    first commit

---
+++ resources/views/tweet/index.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>つぶやきアプリ</title>
+</head>
+
+<body>
+    <h1>つぶやきアプリ</h1>
+    @auth
+    <p>投稿フォーム</p>
+    @if (session('feedback.success')) {
+    <p style="color: green">{{ feedback.success }}</p>
+    }
+    @endif
+    <form action="{{ route('tweet.create') }}" method="post">
+        @csrf
+        <label for="tweet-content">つぶやき</label>
+        <span>140文字</span>
+        <textarea name="tweet" id="tweet-content" type="text" placeholder="つぶやきを入力"></textarea>
+        @error('tweet')
+        <p style="color: red;">{{ $message }}</p>
+        @enderror
+        <button type="submit">投稿</button>
+    </form>
+    @endauth
+    <div>
+        @foreach($tweets as $tweet)
+        <details>
+            <summary>{{ $tweet->content }} by {{ $tweet->user->name }}</summary>
+            <div>
+                <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit">削除</button>
+                </form>
+            </div>
+        </details>
+        @endforeach
+    </div>
+</body>
+
+</html>
